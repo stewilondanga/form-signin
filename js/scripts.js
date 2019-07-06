95ecb9199eff6c985b847a3c0a5ca133cdc84b6f
Stewart ilondanga <stilondanga@gmail.com>
2019-07-06 19:19:28 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -17,15 +17,15 @@ const patterns = {
   email: /^([a-z\d\.-_]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
 }
 
-/*function validate(field, regex){
-   if(regex.test(field.value) || field.value == ''){
-      field.className = "valid";
-   } else {
-      field.className = "invalid";
-   }
+function validate(field, regex) {
+  if (regex.test(field.value) || field.value == '') {
+    field.className = "valid";
+  } else {
+    field.className = "invalid";
+  }
 }
 
-inputs.forEach((input) => {
+/*inputs.forEach((input) => {
    input.addEventListener('keyup', (e) => {
       validate(e.target, patterns[e.target.attributes.name.value])
    });
