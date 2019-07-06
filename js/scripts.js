41fd1c4552e1a762d3b0d7a6debe34a711941806
Stewart ilondanga <stilondanga@gmail.com>
2019-07-06 19:23:25 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -41,11 +41,11 @@ function nextSection() {
   next.classList.add('hide');
 }
 
-/*var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+var navigate = (function() {
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
